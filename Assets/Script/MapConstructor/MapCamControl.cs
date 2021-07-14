370dcd253eed51d3ae69017200052d284a0409b8
михаил белоногов <friman-005@mail.ru>
2021-07-14 09:27:42 +0500

    Добавил поддержку колёсика мышки для маштабированяи карты

--- Assets/Script/MapConstructor/MapCamControl.cs
+++ Assets/Script/MapConstructor/MapCamControl.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class MapCamControl : MonoBehaviour
 {
+    private Camera Cam;
     public float speed =1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cam = GetComponent<Camera>(); 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * speed;
+        float translation = Input.GetAxis("Vertical") * Cam.orthographicSize/30;
+        float rotation = Input.GetAxis("Horizontal") * Cam.orthographicSize/30;
+
+        float mw = -Input.GetAxis("Mouse ScrollWheel");
+        if(mw < 0)
+        {
+            if(Cam.orthographicSize > 1)
+            {
+                Cam.orthographicSize += mw;
+            }
+            else
+            {
+                Cam.orthographicSize = 1;
+            }
+        }else if (mw > 0)
+        {
+            if (Cam.orthographicSize < 10)
+            {
+                Cam.orthographicSize += mw;
+            }
+            else
+            {
+                Cam.orthographicSize = 10;
+            }
+        }
 
         gameObject.transform.position += new Vector3(rotation, translation, 0);
     }
